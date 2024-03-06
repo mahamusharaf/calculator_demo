93180b97672a308b1a374ed4573965eba42f8d83
ayemish <ayemish123@gmail.com>
2024-03-06 11:31:16 +0500

    added substraction functionality

---
+++ sub.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int sub (int a ,int b)
+            {
+                    int c= a-b;
+                    return c;
+            }
+int main()
+{
+    int a =5;
+    int b =2;
+
+}
